7110cfb5c21bcc283e928fb7028f199f0e2b714d
Navan005 <navandeep0511@gmail.com>
2020-09-10 20:13:49 -0400

    Login, Signup,Product Activity

--- app/src/main/java/com/example/getfit/LoginActivity.java
+++ app/src/main/java/com/example/getfit/LoginActivity.java
@@ -2,13 +2,49 @@ package com.example.getfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+    private TextView forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        joinNowButton=(Button) findViewById(R.id.signup_btn);
+        loginButton=(Button) findViewById(R.id.btnLogin);
+        forgotPassword=(TextView) findViewById(R.id.forget_Password);
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this, ProductActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this, ForgotpasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
