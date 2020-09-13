30d5a98b4ccd38ad617410385d24e0ffd2e17029
Navan005 <navandeep0511@gmail.com>
2020-09-13 15:47:52 -0400

    database connectivity at login, signup

--- app/src/main/java/com/example/getfit/LoginActivity.java
+++ app/src/main/java/com/example/getfit/LoginActivity.java
@@ -1,19 +1,30 @@
 package com.example.getfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button joinNowButton, loginButton;
     private TextView forgotPassword;
+    EditText mEmail,mPassword;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,9 @@ public class LoginActivity extends AppCompatActivity {
         joinNowButton=(Button) findViewById(R.id.signup_btn);
         loginButton=(Button) findViewById(R.id.btnLogin);
         forgotPassword=(TextView) findViewById(R.id.forget_Password);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        fAuth = FirebaseAuth.getInstance();
 
         joinNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +49,41 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(LoginActivity.this, ProductActivity.class);
-                startActivity(intent);
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),ProductActivity.class));
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
             }
+
         });
 
         forgotPassword.setOnClickListener(new View.OnClickListener() {
